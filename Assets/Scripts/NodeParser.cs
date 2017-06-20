0df56115582190d48f4cd6c23fd264fe9f922403
Namr <amrmoussa100@gmail.com>
2017-06-20 19:28:54 -0400

    Initial Commit

---
+++ Assets/Scripts/NodeParser.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using UnityEngine;
+
+public class NodeParser : MonoBehaviour {
+
+    char[] delimiterChars = {'	', '	'};
+    public Transform nodeTemplate;
+    public Transform connectionTemplate;
+    // Use this for initialization
+    void Start()
+    {
+        List<string> VNodes = new List<string>();
+        List<Transform> Nodes = new List<Transform>();
+
+        using (StreamReader reader = new StreamReader("Node_AAL90.node"))
+        {
+            
+            string line;
+            line = reader.ReadLine();
+            while ((line = reader.ReadLine()) != null)
+            {
+                VNodes.Add(line); 
+            }
+        }
+        
+        foreach(string vNode in VNodes)
+        {
+            string[] properties = vNode.Split(delimiterChars);
+            float x, y, z, color, size;
+            x = float.Parse(properties[0], CultureInfo.InvariantCulture.NumberFormat);
+            y = float.Parse(properties[1], CultureInfo.InvariantCulture.NumberFormat);
+            z = float.Parse(properties[2], CultureInfo.InvariantCulture.NumberFormat);
+            color = float.Parse(properties[3], CultureInfo.InvariantCulture.NumberFormat);
+            size = float.Parse(properties[4], CultureInfo.InvariantCulture.NumberFormat) * 2;
+            Transform node = (Transform) Instantiate(nodeTemplate, new Vector3(x,y,z), Quaternion.identity);
+            float vectorScale = node.localScale.x + size;
+            node.localScale = new Vector3(vectorScale, vectorScale, vectorScale);
+            node.name = properties[5];
+            node.parent = this.transform;
+            node.tag = "Node";
+            switch ((int)color)
+            {
+                case 1:
+                    node.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 2:
+                    node.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                    break;
+                case 3:
+                    node.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+                    break;
+                case 4:
+                    node.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                    break;
+                case 5:
+                    node.gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+                    break;
+            }
+            Nodes.Add(node);
+        }
+
+        List<string> NodeConnections = new List<string>();
+       
+        using (StreamReader reader = new StreamReader("Edge_AAL90_Binary.edge"))
+        {
+            string line;
+            line = reader.ReadLine();
+            Debug.Log(line);
+            while ((line = reader.ReadLine()) != null)
+            {
+                NodeConnections.Add(line);
+            }
+        }
+
+        int nodeCount = 0;
+        foreach (string nodeConnection in NodeConnections)
+        {
+            string[] properties = nodeConnection.Split(delimiterChars);
+            int Connectioncount = 0;
+            foreach(string s in properties)
+            {
+                if(int.Parse(s) == 1)
+                {
+                    Transform connection = (Transform)Instantiate(connectionTemplate, Nodes[nodeCount].position, Quaternion.identity);
+                    Vector3 connectionDistance = Nodes[nodeCount].position - Nodes[Connectioncount].position;
+                    connection.position = Nodes[nodeCount].position;
+                    connection.localScale = new Vector3(connection.localScale.x, connection.localScale.y, connectionDistance.magnitude);
+                    connection.LookAt(Nodes[Connectioncount].position);
+                    //DrawLine(Nodes[nodeCount].position, Nodes[Connectioncount].position, Color.black, 0.5f);
+                }
+                Connectioncount++;
+            }
+            nodeCount++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float width)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.startColor = color;
+        lr.endColor = color;
+        lr.startWidth = width;
+        lr.endWidth = width;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        myLine.transform.parent = this.transform;
+    }
+}
---
+++ Assets/Scripts/Viewer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viewer : MonoBehaviour {
+
+    float XAngle;
+    float YAngle;
+    float ZAngle;
+
+    float forwardMovement;
+    float sideMovement;
+
+    float Speed = 10;
+
+    public Transform Fulcrum;
+    public Transform Brain;
+
+    Vector3 startPos;
+    Vector3 vectorDistance;
+    Quaternion startRot;
+	// Use this for initialization
+	void Start ()
+    {
+        startPos = transform.position;
+        startRot = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        vectorDistance = transform.position - Brain.position;
+        Speed = vectorDistance.magnitude * 0.2f;
+        YAngle = Input.GetAxis("Oculus_Touch_RThumbstickX") * Speed * Time.deltaTime;
+        XAngle = Input.GetAxis("Oculus_Touch_RThumbstickY") * Speed * Time.deltaTime;
+
+        
+        if(Input.GetButton("Fire2"))
+        {
+            sideMovement = Input.GetAxis("Oculus_Touch_LThumbstickX") * Speed * Time.deltaTime;
+        }
+        else
+        {
+            ZAngle = Input.GetAxis("Oculus_Touch_LThumbstickX") * Speed * Time.deltaTime;
+            sideMovement = 0;
+        }
+
+        forwardMovement = Input.GetAxis("Oculus_Touch_LThumbstickY") * Speed * Time.deltaTime;
+
+        transform.RotateAround(Fulcrum.position,new Vector3(1.0f,0.0f,0.0f),XAngle);
+        transform.RotateAround(Fulcrum.position, new Vector3(0.0f, 1.0f, 0.0f), YAngle);
+
+        transform.Rotate(new Vector3(0, 0, ZAngle / Speed * 20));
+        transform.Translate(new Vector3(sideMovement, 0.0f, forwardMovement));
+
+        if(Input.GetButtonUp("Fire1"))
+        {
+            XAngle = 0;
+            YAngle = 0;
+            transform.position = startPos;
+            transform.rotation = startRot;
+        }
+    }
+}
