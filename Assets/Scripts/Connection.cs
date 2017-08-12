d48bb05f9fbad11415bb8e55da6bf2aa3d4c2b82
Namr <amrmoussa100@gmail.com>
2017-08-12 18:19:25 -0400

    Video Playback and colored nodes

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection : MonoBehaviour {
+    public float threshold;
+    public NodeParser parser;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(this.transform.parent.gameObject.activeSelf)
+        {
+            if (parser.threshold > threshold)
+            {
+                //this.gameObject.SetActive(false);
+            }
+            else
+            {
+                //this.gameObject.SetActive(true);
+            }
+        }
+
+	}
+}
