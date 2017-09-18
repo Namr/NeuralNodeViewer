d76ab14399101ccb8518e54b7dd9527ebff10d92
Namr <amrmoussa100@gmail.com>
2017-09-18 18:23:34 -0400

    Commented Splitter File and made Slicer File

---
+++ Assets/Scripts/BrainMeshSlicing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainMeshSlicing : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    
+    public void Slice()
+    {
+        MeshFilter mf = transform.GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        mf.mesh = mesh;
+
+    }
+}
--- Assets/Scripts/BrainMeshSplitter.cs
+++ Assets/Scripts/BrainMeshSplitter.cs
@@ -17,34 +17,42 @@ public class BrainMeshSplitter : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        //init the components that will be attached to the game object
         MeshFilter mf = transform.GetComponent<MeshFilter>();
         Mesh mesh = new Mesh();
         mf.mesh = mesh;
         using (StreamReader reader = new StreamReader(filename))
         {
+            //set up varibles to hold the vertecies of the mesh
+            
+            //this is the number of vertices in the mesh file
             int vertexNumber = int.Parse(reader.ReadLine());
-
+            //the vertices in string and vector form of the original file
             Vector3[] vertices = new Vector3[vertexNumber];
             string[] rawVertices = new string[vertexNumber];
-
+            //these are the arrays that will hold the vertices after their split
             Vector3[] posVertices = new Vector3[vertexNumber];
             Vector3[] negVertices = new Vector3[vertexNumber];
 
-
+            //for each index(vertex) is it above or bellow the bound set
             bool[] isPositive = new bool[vertexNumber];
+            //this varible stores new the index for every vertex in the old file
             int[] newIndex = new int[vertexNumber];
+
+            //populate the arrays of the original mesh file by parsing the mesh file
             for (int i = 0; i < vertexNumber; i++)
             {
                 rawVertices[i] = reader.ReadLine();
                 string[] properties = rawVertices[i].Split(delimiterChars);
                 vertices[i] = new Vector3(float.Parse(properties[0]), float.Parse(properties[1]), float.Parse(properties[2]));
             }
-
+            //activate if we have passed the vertex limit
             if (vertexNumber > 65000)
             {
                 int posCount = 0;
                 int negCount = 0;
                 int count = 0;
+                //go through every vertex and see which child mesh it belongs in and place it there, also keep track of the indexes
                 foreach (Vector3 vertex in vertices)
                 {
                     if (vertex.x > -3)
@@ -67,6 +75,7 @@ public class BrainMeshSplitter : MonoBehaviour
                 posVertices = posVertices.Where(c => c != Vector3.zero).ToArray();
                 negVertices = negVertices.Where(c => c != Vector3.zero).ToArray();
 
+                //init child mesh
                 Transform brainChild = (Transform)Instantiate(BrainMesh, new Vector3(0, 0, 0), Quaternion.identity);
                 Mesh childMesh = new Mesh();
                 brainChild.parent = this.transform.parent;
@@ -74,7 +83,7 @@ public class BrainMeshSplitter : MonoBehaviour
 
                 mesh.vertices = posVertices;
                 childMesh.vertices = negVertices;
-
+                //number of triangles
                 int triNumber = int.Parse(reader.ReadLine());
                 int[] posTriangles = new int[triNumber * 3];
                 int[] negTriangles = new int[triNumber * 3];
@@ -84,11 +93,11 @@ public class BrainMeshSplitter : MonoBehaviour
                 {
                     string triData = reader.ReadLine();
                     string[] properties = triData.Split(delimiterChars);
-
+                    //the original index of the three vertices of the triangles
                     int index0 = int.Parse(properties[0]) - 1;
                     int index1 = int.Parse(properties[1]) - 1;
                     int index2 = int.Parse(properties[2]) - 1;
-
+                    //if all vertices of the triangle are positive, find their new index and make a traingle
                     if(isPositive[index0] && isPositive[index1] && isPositive[index2])
                     {
                         posTriangles[posTriCount] = newIndex[index0];
@@ -98,6 +107,7 @@ public class BrainMeshSplitter : MonoBehaviour
                         posTriangles[posTriCount] = newIndex[index2];
                         posTriCount++;
                     }
+                    //if all vertices of the triangle are negative, find their new index and make a traingle
                     else if (!isPositive[index0] && !isPositive[index1] && !isPositive[index2])
                     {
                         negTriangles[negTriCount] = newIndex[index0];
@@ -108,6 +118,7 @@ public class BrainMeshSplitter : MonoBehaviour
                         negTriCount++;
                     }
                 }
+                //populate the meshes
                 mesh.triangles = posTriangles;
                 childMesh.triangles = negTriangles;
             }
